ff20b63cd3558c26cad3defbd7b228d231f7168c
rfdornelles <rodornelles@gmail.com>
2020-09-12 00:55:48 -0300

    Começando a organizar a base da SSP para fazer comparação com COVID

--- data-raw/SSP.R
+++ data-raw/SSP.R
@@ -1,39 +1,58 @@
-## correlação espacial
-# https://www.bigbookofr.com/geospatial.html
-
-# https://geocompr.robinlovelace.net/geometric-operations.html
-#remotes::install_github("Nowosad/spDataLarge")
+e #### Carregar base ####
+library(dplyr)
 
 ssp <- readr::read_rds("data/ssp.rds")
-covid <- readr::read_rds("data/covid.rds")
 
-library(dplyr)
-
-covid <- covid %>%
-  filter(estado == "SP", !is.na(municipio), data == max(data))
-
-mapa_sp <- geobr::read_municipality(code_muni = "SP")
-
-library(ggplot2)
-
-mapa_sp %>%
-  ggplot() +
-  geom_sf()
-
-mapa_sp
-
-?sf::st_join
-
-sf::st_join
-
-library(sf)
-library(raster)
-library(dplyr)
-library(spData)
-
-covid_loc <- covid %>%
-  dplyr::select(municipio, codmun, lat, lon)
-
-st_join(mapa_sp, covid_loc)
+####
+ssp %>% View()
 
 
+sobra <- anti_join(ssp %>%
+   select(municipio_nome) %>%
+  unique(), covid_sp %>%
+  select(municipio) %>%
+  unique(),
+  by = c("municipio_nome" = "municipio"))
+
+ssp %>%
+  select(-delegacia_nome, -regiao_nome) %>%
+  rowwise(mes, ano, municipio_nome) %>%
+  mutate(
+    estupro = sum(c_across(contains("estupro")))
+    homicidio =
+         ) %>% View()
+
+ssp %>%
+  rowwise(mes, ano, municipio_nome) %>%
+  mutate(
+    checa_estupro = estupro_total == (estupro + estupro_vulneravel)
+  ) %>%
+  filter(!checa_estupro, (estupro != 0 | estupro_vulneravel != 0)) %>%
+  select(ano, mes, municipio_nome, contains("estupro")) %>%
+  View()
+
+ssp %>%
+  select(ano, mes, municipio_nome, contains("total"))
+
+# Vou usar estupro_total e roubo_total
+
+base <- ssp %>%
+  select(-delegacia_nome, -regiao_nome, roubo = roubo_total) %>%
+  mutate(
+    estupro = estupro_total
+      ) %>%
+  select(-estupro_total, -estupro_vulneravel, -contains("roubo_"))
+
+base %>%
+rowwise(mes, ano, municipio_nome) %>%
+  mutate(
+    furto = sum(c_across(contains("furto"))),
+    homicidio = sum(c_across(contains("hom_"))),
+    lesao_corporal = sum(c_across(contains("lesao_corp"))),
+
+    )
+
+
+    %>% View()
+
+names(ssp)
